49cfe3d008a52969b8b499348fabf368bf0ad64c
GeetanshJ <geetansh0513.be21@chitkara.edu.in>
2024-12-02 18:01:19 +0530

    created client page of zwigato

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home/Home';
+import Cart from './pages/Cart/Cart';
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
+import Footer from './components/Footer/Footer';
+import { useState } from 'react';
+import Login from './components/Login/Login';
+
+function App() {
+
+  const[login,setLogin] = useState(false);
+  return (
+    <>
+    {login ? <Login setLogin={setLogin}/>:<></>}
+
+    <div className="App">
+      <Navbar setLogin = {setLogin}/>
+      <Routes>
+        <Route path = "/" element={<Home/>}/>
+        <Route path = "/cart" element={<Cart/>}/>
+        <Route path = "/order" element={<PlaceOrder/>}/>
+      </Routes>
+    </div>
+
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
